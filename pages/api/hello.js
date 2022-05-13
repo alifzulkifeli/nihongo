b1ff2da6c03f1168ebf14ae80d4c63f7ab117481
aliff <alifzulkifeli@gmail.com>
2022-05-13 22:44:06 +0800

    asd

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const resd = await fetch('http://nihongo-api.alifzulkifeli.com/random')
-  const data = await resd.json()
-  res.status(200).json({ data})
+
+  res.status(200).json({ e:"sss"})
 }
